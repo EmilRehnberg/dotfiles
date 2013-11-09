68d6e342e914cac1cfab1289048a521aaa51d786
Emil Rehnberg <emil.rehnberg@gmail.com>
2013-11-09 22:53:55 +0100

    linkセットアップを完成しました。vimはまだ壊れた。

--- run-linking-setup.sh
+++ run-linking-setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# 目的：dotfilesリンクを作れています。
 
 dotfiles_dir=~/dotfiles
 old_dotfiles_dir=~/.dotfiles_old
@@ -8,20 +9,20 @@ echo "Creating ${old_dotfiles_dir} for backup of any existing dotfiles in ~"
 mkdir -p ${old_dotfiles_dir}
 
 mkdir -p ${dotfiles_dir}
-cd ${dotfiles_dir}
+cd 
 
 echo "Moving any existing dotfiles from ~ to ${old_dotfiles_dir}"
 for dotfile in ${dotfiles}; do
-  dotfile_path = "~/.${dotfile}"
+  dotfile_path=".${dotfile}"
   if [ -e ${dotfile_path} ] ; then
     if [ -h ${dotfile_path} ] ; then
       echo "Deleting symbolic link ${dotfile_path}"
-      #rm ${dotfile_path}
+      rm ${dotfile_path}
     else 
       echo "Moving ${dotfile_path} to ${old_dotfiles_dir}"
-      #mv ${dotfile_path} ${old_dotfiles_dir}
+      mv ${dotfile_path} ${old_dotfiles_dir}
     fi
   fi
   echo "Creating symbolic link to ${dotfile} in home directory."
-  #ln -s ${dotfiles_dir}/${dotfile} ${dotfile_path}
+  ln -s ${dotfiles_dir}/${dotfile} ${dotfile_path}
 done
