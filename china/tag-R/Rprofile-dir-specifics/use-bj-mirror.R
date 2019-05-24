3d1bd53a64296dff9d078e02f5165effa47b44ff
Emil Rehnberg <emil.rehnberg@gmail.com>
2019-05-24 20:30:59 +0200

    R: tidy use-bj-mirror script

--- china/tag-R/Rprofile-dir-specifics/use-bj-mirror.R
+++ china/tag-R/Rprofile-dir-specifics/use-bj-mirror.R
@@ -1,5 +1,3 @@
 #!/usr/bin/env R
 # sets default repo to beijing
-getOption("repos") %T>%
-  { .["CRAN"] <- "http://mirror.bjtu.edu.cn/cran/" } %>%
-  { options(repos = .) }
+options(repos = structure(c(CRAN = "http://mirror.bjtu.edu.cn/cran/")))
