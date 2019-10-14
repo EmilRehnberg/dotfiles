13540e918514409f14a3631d17bb4c0f987914f9
Emil Rehnberg <emil.rehnberg@gmail.com>
2019-10-14 08:53:36 +0200

    zsh: add omz colorize style
    - not sure if it's working

--- tag-zsh/zsh-tag-specific/zshrc/load-omz.sh
+++ tag-zsh/zsh-tag-specific/zshrc/load-omz.sh
@@ -4,4 +4,5 @@ export UPDATE_ZSH_DAYS=2
 ZSH_THEME="simple"                                                       # ~/.oh-my-zsh/themes/
 export plugins=(command-not-found colorize colored-man-pages git-prompt) # ~/.oh-my-zsh/plugins
 sourceDirectory "$HOME/.omz-tag-specific/plugins"
+ZSH_COLORIZE_STYLE="colorful"
 source $ZSH/oh-my-zsh.sh
