c153a6fa29b57aad388958b2cf5915f8ad6997c6
Emil Rehnberg <emil.rehnberg@gmail.com>
2019-10-14 08:44:49 +0200

    zsh: append to omz plugins via custom dir
    - useful to combine with tags

---
+++ tag-zsh/omz-tag-specific/plugins/example.sh
@@ -0,0 +1,1 @@
+# export plugins=($plugins example-plugin)
--- tag-zsh/zsh-tag-specific/zshrc/load-omz.sh
+++ tag-zsh/zsh-tag-specific/zshrc/load-omz.sh
@@ -3,4 +3,5 @@ export ZSH=$HOME/.oh-my-zsh
 export UPDATE_ZSH_DAYS=2
 ZSH_THEME="simple" # other themes: ~/.oh-my-zsh/themes/
 plugins=(bundler common-aliases debian git rails ruby screen tmux) # more plugins: ~/.oh-my-zsh/plugins
+sourceDirectory "$HOME/.omz-tag-specific/plugins"
 source $ZSH/oh-my-zsh.sh
