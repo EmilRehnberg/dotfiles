4b634752be62d5f3769551dd9e24075b7f078f96
Emil Rehnberg <emil.rehnberg@gmail.com>
2019-10-14 08:51:48 +0200

    zsh: update omz plugins

--- tag-zsh/zsh-tag-specific/zshrc/load-omz.sh
+++ tag-zsh/zsh-tag-specific/zshrc/load-omz.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 export ZSH=$HOME/.oh-my-zsh
 export UPDATE_ZSH_DAYS=2
-ZSH_THEME="simple" # other themes: ~/.oh-my-zsh/themes/
-plugins=(bundler common-aliases debian git rails ruby screen tmux) # more plugins: ~/.oh-my-zsh/plugins
+ZSH_THEME="simple"                                                       # ~/.oh-my-zsh/themes/
+export plugins=(command-not-found colorize colored-man-pages git-prompt) # ~/.oh-my-zsh/plugins
 sourceDirectory "$HOME/.omz-tag-specific/plugins"
 source $ZSH/oh-my-zsh.sh
