433cbf87c6a5e77b81a15a22a14bde3e258924a1
Emil Rehnberg <emil.rehnberg@gmail.com>
2019-12-22 20:05:38 +0100

    zsh: add sh shebang to private-env

--- tag-private/zsh-tag-specific/zshrc/private-env.sh
+++ tag-private/zsh-tag-specific/zshrc/private-env.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env sh
 alias yt='youtube-dl -t'
 alias yt_dl_mp3='youtube-dl --extract-audio --audio-format mp3 --audio-quality 320K --add-metadata'
 alias walkman='mpv --no-audio-display --no-video'
