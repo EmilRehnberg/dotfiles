8400654c1e33f7383031d4d1e6bf98e15fb05da5
Emil Rehnberg <emil.rehnberg@gmail.com>
2019-12-22 19:42:35 +0100

    python: mk conda-env POSIX compliant

--- tag-python/zsh-tag-specific/zshenv/conda-env.sh
+++ tag-python/zsh-tag-specific/zshenv/conda-env.sh
@@ -1,14 +1,14 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-__conda_setup="$('/home/e/.anaconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
-if [ $? -eq 0 ]; then
+__conda_setup="$("$HOME"'/.anaconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+if test "$?" = 0 ; then
     eval "$__conda_setup"
 else
-    if [ -f "/home/e/.anaconda3/etc/profile.d/conda.sh" ]; then
-        . "/home/e/.anaconda3/etc/profile.d/conda.sh"
+    if test -f "$HOME/.anaconda3/etc/profile.d/conda.sh" ; then
+        . "$HOME/.anaconda3/etc/profile.d/conda.sh"
     else
-        export PATH="/home/e/.anaconda3/bin:$PATH"
+        export PATH="$HOME/.anaconda3/bin:$PATH"
     fi
 fi
 unset __conda_setup
