0f1b9bb340627e5867d5d664af78b186d186f5f4
Emil Rehnberg <emil.rehnberg@gmail.com>
2020-04-13 20:23:54 +0200

    add gitfast zsh plugin
    = WHY =
    
    the gitfast plugin improves the autocompletion for git including switch
    and restore commands

--- tag-zsh/config/sh/interactive/load-omz.sh
+++ tag-zsh/config/sh/interactive/load-omz.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 export ZSH="$HOME/.oh-my-zsh"
 export UPDATE_ZSH_DAYS=2
-ZSH_THEME="simple"                                                                          # ~/.oh-my-zsh/themes/
-export plugins=(command-not-found colorize colored-man-pages custom-completions git-prompt) # ~/.oh-my-zsh/plugins
+ZSH_THEME="simple" # ~/.oh-my-zsh/themes/
+export plugins=(command-not-found colorize colored-man-pages custom-completions gitfast git-prompt) # ~/.oh-my-zsh/plugins
 ZSH_CUSTOM="$XDG_CONFIG_HOME/omz"
 sourceDirectory "$ZSH_CUSTOM/load-env"
 ZSH_COLORIZE_STYLE="colorful"
