23d1abd31a6c93f052a7a64830f2e702903aa2bc
Emil Rehnberg <emil.rehnberg@gmail.com>
2023-06-13 14:29:12 +0200

    refactor conda env
    use the XDG environment variables for both ubuntu and macos

--- tag-python/config/sh/interactive/conda-env.sh
+++ tag-python/config/sh/interactive/conda-env.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env sh
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-__conda_setup="$("$XDG_DATA_HOME"'/miniconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+CONDA_USER_HOME="$XDG_DATA_HOME/miniconda3" # XDG vars on ubuntu/macos
+__conda_setup="$("$CONDA_USER_HOME"'/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
 if [ $? -eq 0 ]; then
     eval "$__conda_setup"
 else
-    if [ -f "$XDG_DATA_HOME/miniconda3/etc/profile.d/conda.sh" ]; then
-        . "$XDG_DATA_HOME/miniconda3/etc/profile.d/conda.sh"
+    if [ -f "$CONDA_USER_HOME/etc/profile.d/conda.sh" ]; then
+        . "$CONDA_USER_HOME/etc/profile.d/conda.sh"
     else
-        export PATH="$XDG_DATA_HOME/miniconda3/bin:$PATH"
+        export PATH="$CONDA_USER_HOME/bin:$PATH"
     fi
 fi
 unset __conda_setup
