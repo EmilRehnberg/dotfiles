6d9bff0b4f00acfab0ece64e0fbbd440434c7f3d
Emil Rehnberg <emil.rehnberg@gmail.com>
2023-08-02 21:49:38 +0200

    add conda path logic as last interactive script
    1. remove 00-python-env in favor of conda
    2. prefix name of conda-env with zlast to make sure it runs last

--- macos/tag-python/config/sh/interactive/00-python-env.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-BREW_PREFIX=$(brew --prefix)
-export PATH="$BREW_PREFIX/opt/python/libexec/bin:$PATH"
--- tag-python/config/sh/interactive/zlast-conda-env.sh
+++ tag-python/config/sh/interactive/zlast-conda-env.sh
@@ -1,6 +1,20 @@
 #!/usr/bin/env sh
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
+
+CONDA_SYSTEM_HOME="/opt/homebrew/Caskroom/miniconda/base"
+__conda_setup="$("$CONDA_SYSTEM_HOME"'/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "$CONDA_SYSTEM_HOME/etc/profile.d/conda.sh" ]; then
+        . "$CONDA_SYSTEM_HOME/etc/profile.d/conda.sh"
+    else
+        export PATH="$CONDA_SYSTEM_HOME/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+
 CONDA_USER_HOME="$XDG_DATA_HOME/miniconda3" # XDG vars on ubuntu/macos
 __conda_setup="$("$CONDA_USER_HOME"'/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
 if [ $? -eq 0 ]; then
@@ -13,3 +27,4 @@ else
     fi
 fi
 unset __conda_setup
+# <<< conda initialize <<<
