35faeeee80805c028d4a6a8387828f669917bf1a
Emil Rehnberg <emil.rehnberg@gmail.com>
2023-08-08 09:13:42 +0200

    adjust python paths to work with conda
    in vi and shell (with conda)

--- macos/config/nvim/plugin/macos-python.vim
+++ macos/config/nvim/plugin/macos-python.vim
@@ -1,2 +1,1 @@
-let g:python2_host_prog = '/usr/local/Cellar/python@2/2.7.16_1/bin/python2'
-let g:python3_host_prog = '/usr/local/Cellar/python/3.7.5/bin/python3'
+" empty, used to have set python paths, bad idea with envs
--- tag-python/config/sh/interactive/zlast-conda-env.sh
+++ tag-python/config/sh/interactive/zlast-conda-env.sh
@@ -15,16 +15,4 @@ else
 fi
 unset __conda_setup
 
-CONDA_USER_HOME="$XDG_DATA_HOME/miniconda3" # XDG vars on ubuntu/macos
-__conda_setup="$("$CONDA_USER_HOME"'/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
-if [ $? -eq 0 ]; then
-    eval "$__conda_setup"
-else
-    if [ -f "$CONDA_USER_HOME/etc/profile.d/conda.sh" ]; then
-        . "$CONDA_USER_HOME/etc/profile.d/conda.sh"
-    else
-        export PATH="$CONDA_USER_HOME/bin:$PATH"
-    fi
-fi
-unset __conda_setup
 # <<< conda initialize <<<
